9061177405d6d6e080b219a46e01170ca9be02fe
veidjz <veidjz@proton.me>
2026-07-31 12:01:04 -0300

    feat: connect to mongo using the validated url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { validateEnvironment } from './config/env.validation'
@@ -7,6 +8,13 @@ import { validateEnvironment } from './config/env.validation'
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: validateEnvironment }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGO_URL'),
+        serverSelectionTimeoutMS: 5000,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
