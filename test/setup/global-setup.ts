e6a1fb7355a1b46b6b1b4594001a52dbeb8e252b
veidjz <veidjz@proton.me>
2026-07-31 13:23:04 -0300

    test: honour mongo url override for an external instance

--- test/setup/global-setup.ts
+++ test/setup/global-setup.ts
@@ -1,12 +1,16 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server'
 
-let replicaSet: MongoMemoryReplSet
+let replicaSet: MongoMemoryReplSet | undefined
 
 export default async function startMongo(): Promise<void> {
+  if (process.env.MONGO_URL) {
+    return
+  }
+
   replicaSet = await MongoMemoryReplSet.create()
   process.env.MONGO_URL = replicaSet.getUri('inmeta')
 }
 
 export async function stopMongo(): Promise<void> {
-  await replicaSet.stop()
+  await replicaSet?.stop()
 }
